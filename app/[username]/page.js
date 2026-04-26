2db98831c0d3811edb62536dabfe49d4f6388750
Aji muhamad Jaelani <ajimjaelani04@email.com>
2026-04-26 20:05:37 +0700

    update homepage

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,13 +1,36 @@
-export default function Home() {
+import { supabase } from '../../lib/supabase'
+
+export default async function Page({ params }) {
+  const { username } = await params
+
+  // cari santri
+  const { data: santri } = await supabase
+    .from('santri')
+    .select('*')
+    .eq('username', username)
+    .single()
+
+  if (!santri) {
+    return <div>Santri tidak ditemukan</div>
+  }
+
+  // ambil karya
+  const { data: karya } = await supabase
+    .from('karya')
+    .select('*')
+    .eq('santri_id', santri.id)
+
   return (
     <div style={{ padding: 20 }}>
-      <h1>PORTOFOLIO SANTRI 🔥</h1>
-      <p>Contoh buka halaman santri:</p>
+      <h1>{santri.nama}</h1>
 
-      <ul>
-        <li><a href="/aji">/aji</a></li>
-        <li><a href="/budi">/budi</a></li>
-      </ul>
+      {karya?.map((item) => (
+        <div key={item.id}>
+          <h3>{item.judul}</h3>
+          <p>{item.deskripsi}</p>
+          <a href={item.link}>Lihat</a>
+        </div>
+      ))}
     </div>
   )
 }
